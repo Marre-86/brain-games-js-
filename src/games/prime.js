3427cba858d5e1a6118261f987ec82361dbd6592
Artem Pokhiliuk <timtimoha@yandex.ru>
2023-04-26 13:56:34 +0600

    Small logic fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const isPrime = (num) => {
-  for (let i = 2; i < (num / 2); i += 1) {
+  for (let i = 2; i <= (num / 2); i += 1) {
     if (num % i === 0) {
       return false;
     }
